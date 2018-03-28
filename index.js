437eb14f1bff68c1beb2f4f357b6644a1a020dcf
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-03-28 22:55:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(recipe, key, value) {
+  recipe[key] = value
+  return recipe
+}
+
+function updateObjectWithKeyAndValue(recipe, key, value) {
+  return Object.assign({}, recipe, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipe, key, value) {
+  recipe[key] = value
+  return recipe
+}
+
+function deleteFromObjectByKey(recipe, key) {
+  newRecipe = Object.assign({}, recipe)
+  delete newRecipe[key]
+  return newRecipe
+}
+
+function deleteFromObjectByKey(recipe, key) {
+  delete recipe[key]
+  return recipe
+}
+
