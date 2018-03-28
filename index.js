a09c27cb34854b0f3649a362cada8f9bf37ed029
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-03-28 22:56:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ function destructivelyUpdateObjectWithKeyAndValue(recipe, key, value) {
 function deleteFromObjectByKey(recipe, key) {
   newRecipe = Object.assign({}, recipe)
   delete newRecipe[key]
-  return newRecipe
   return recipe
 }
 
